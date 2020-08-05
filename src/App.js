6b7d3bfd944d045eaac3ccd13f4383f2f0d04291
Kuliza-280 <psnitsri01@gmail.com>
2020-08-05 18:45:37 +0530

    Controlling Input State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
 
+import logoSrc from "./logo.png";
 import "./App.css";
 
 function App() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleLogin = () => {
+    console.log({ email, password });
+  };
+
   return (
     <div className="wrapper">
       <header className="header">
-        <img
-          className="logo-header"
-          src="https://s3.ap-south-1.amazonaws.com/tpng-images/random/original/6be52f0b-56bf-4fdc-abd3-0bec9e58610c.png"
-          alt="Healthify Logo"
-        />
+        <img className="logo-header" src={logoSrc} alt="Healthify Logo" />
         <p className="title">Accounts</p>
       </header>
       <div className="container">
         <div className="login-view">
-          <img
-            className="logo-login"
-            src="https://s3.ap-south-1.amazonaws.com/tpng-images/random/original/6be52f0b-56bf-4fdc-abd3-0bec9e58610c.png"
-            alt="Healthify Logo"
-          />
+          <img className="logo-login" src={logoSrc} alt="Healthify Logo" />
           <h3>Sign In</h3>
           <p>Use your HealthifyMe Account</p>
           <form>
@@ -27,15 +27,25 @@ function App() {
               className="input"
               type="email"
               name="email"
+              value={email}
               placeholder="Enter Your Email"
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               className="input"
               type="password"
               name="password"
+              value={password}
               placeholder="Enter Your Password"
+              onChange={(e) => setPassword(e.target.value)}
             />
-            <input className="input" type="button" value="Login" />
+
+            <input
+              className="input btn"
+              onClick={handleLogin}
+              type="button"
+              value="Login"
+            />
           </form>
         </div>
       </div>
