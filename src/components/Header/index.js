f3a98beee55a1b098fb813bec530902b703de49b
Kuliza-280 <psnitsri01@gmail.com>
2020-08-14 13:25:56 +0530

    Updating assets and Project Name

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import logoSrc from "../../assets/logo.png";
+import logoSrc from "../../assets/logo.svg";
 import "./header.css";
 
 function Header({ title = "" }) {
   return (
     <header className="header">
-      <img className="logo-header" src={logoSrc} alt="Healthify Logo" />
+      <img className="logo-header" src={logoSrc} alt="Dev World Logo" />
       <p data-testid="header-title" className="title">
         {title}
       </p>
--- src/screens/App/index.js
+++ src/screens/App/index.js
@@ -4,7 +4,7 @@ import validate from "../../utils/validate";
 import Layout from "../../components/Layout";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
-import logoSrc from "../../assets/logo.png";
+import logoSrc from "../../assets/logo.svg";
 import "./app.css";
 
 function App() {
@@ -55,11 +55,11 @@ function App() {
   };
 
   return (
-    <Layout title="Accounts">
+    <Layout title="Login">
       <form className="login-view" onSubmit={handleLogin}>
-        <img className="logo-login" src={logoSrc} alt="Healthify Logo" />
+        <img className="logo-login" src={logoSrc} alt="Dev World Logo" />
         <h3>Sign In</h3>
-        <p>Use your HealthifyMe Account</p>
+        <p>Use your Dev World Account</p>
         {success && <p className="success">{success}</p>}
         <Input
           type="email"
@@ -83,7 +83,11 @@ function App() {
           data-testid="password"
         />
 
-        <Button loading={loading} title="Login" onClick={handleLogin} />
+        <Button
+          loading={loading}
+          title={loading ? "Logging" : "Login"}
+          onClick={handleLogin}
+        />
       </form>
     </Layout>
   );
