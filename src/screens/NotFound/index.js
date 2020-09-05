7a296fab7922c6b9f517934f2a286c8032e9c5f7
Kuliza-280 <psnitsri01@gmail.com>
2020-09-05 13:03:24 +0530

    Adding Not Found Screen for no matched path

---
+++ src/screens/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Layout from "../../components/Layout";
+
+function NotFound(props) {
+  return <Layout title="404">Not Found</Layout>;
+}
+
+NotFound.propTypes = {};
+
+export default NotFound;
