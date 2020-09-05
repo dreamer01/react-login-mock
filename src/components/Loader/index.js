a81508a64090fbb51f618d3f95b76ef64b9bc817
Kuliza-280 <psnitsri01@gmail.com>
2020-09-05 13:02:54 +0530

    Adding Loader

---
+++ src/components/Loader/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./loader.css";
+
+function Loader() {
+  return (
+    <div className="spinner-view">
+      <div className="spinner-circle spinner" />
+    </div>
+  );
+}
+
+export default Loader;
