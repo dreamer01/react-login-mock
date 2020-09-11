4088d6f28eafb3bd16b15e7ea3f87d9bc9b99610
Kuliza-280 <psnitsri01@gmail.com>
2020-09-11 16:38:15 +0530

    Updating Router to BrowserRouter

--- src/utils/testing.js
+++ src/utils/testing.js
@@ -1,33 +1,25 @@
 import React from "react";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 import { render as rtlRender } from "@testing-library/react";
 
-function render(
-  ui,
-  {
-    route = "/",
-    history = createMemoryHistory({ initialEntries: [route] }),
-    ...renderOptions
-  } = {}
-) {
-  function Wrapper({ children }) {
-    const { location, ...navigator } = history;
-    return (
-      <Router location={location} navigator={navigator}>
-        {children}
-      </Router>
+function render(ui, { route = "/", ...renderOptions } = {}) {
+  if (route !== "/login")
+    window.localStorage.setItem(
+      "@test:auth",
+      JSON.stringify({ isLogged: true })
     );
+  const history = createMemoryHistory();
+  history.push(route);
+
+  function Wrapper({ children }) {
+    return <Router history={history}>{children}</Router>;
   }
   return {
     ...rtlRender(ui, {
       wrapper: Wrapper,
       ...renderOptions,
     }),
-    // adding `history` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
-    history,
   };
 }
 
