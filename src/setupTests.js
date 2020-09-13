1d30f05e9458abea6957a1acfe714c6d878a88fc
Kuliza-280 <psnitsri01@gmail.com>
2020-09-13 16:51:19 +0530

    Configuring cypress

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,8 +4,10 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
 import { setupServer } from "msw/node";
-
+import MutationObserver from "@sheerun/mutationobserver-shim";
 import handlers from "./utils/handlers";
+
+window.MutationObserver = MutationObserver;
 
 const server = setupServer(...handlers);
 
